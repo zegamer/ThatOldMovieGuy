effb07d03eb604113d8dbeae36e2b69c42521e9e
AndrewMahandrew <mahandra.raditya@gmail.com>
2020-02-18 15:21:14 +0100

    Tweet generator

--- twitter_demo.py
+++ twitter_demo.py
@@ -19,11 +19,16 @@ $ pip install python-twitter
 
 import time
 import twitter
+
 #for docs, see https://python-twitter.readthedocs.io/en/latest/twitter.html
 
 #speech generator
 import random
 
+api = twitter.Api(consumer_key='77fIyHnx653Nnx0W4Iz4XRua9',
+                      consumer_secret='rG0FQNYliP2jlab2Nf0VYm83a3iC0UUFmyOetHd8aaD4nSa4aE',
+                      access_token_key='1227546694969167873-6Feip4gF0vg0DJxN38yLWCrPIVNYVt',
+                      access_token_secret='rOozbozQIqpy5JonuSrlkxq7d4NXSeIjhUCjtotVgZeHJ')
 
 def twitter_demo():
 
@@ -33,10 +38,6 @@ def twitter_demo():
     #                   access_token_key='972389995083317248-W71G8021PU4IEsIYKbak7dznEhbLx8w',
     #                   access_token_secret='rMcuzSrplE1B9O0jCw9vwVAJz50xzMAA6bOAU3QEOTvXn')
     #
-    api = twitter.Api(consumer_key='77fIyHnx653Nnx0W4Iz4XRua9',
-                      consumer_secret='rG0FQNYliP2jlab2Nf0VYm83a3iC0UUFmyOetHd8aaD4nSa4aE',
-                      access_token_key='1227546694969167873-6Feip4gF0vg0DJxN38yLWCrPIVNYVt',
-                      access_token_secret='rOozbozQIqpy5JonuSrlkxq7d4NXSeIjhUCjtotVgZeHJ')
 
     # get followers
     print("Getting a list of accounts I follow on Twitter...")
@@ -61,7 +62,7 @@ def twitter_demo():
     # this should output: [User(ID=222060384, ScreenName=jd7h)]
 
     #tweeting
-    body = random.choice(["Hey there movie fans, can any of you guys figure out where this quote comes from? ", "I have this quote stuck in my head, can someone help me? ", "Can you guess where this quote came from? ", "Can someone help me find out where this quote is from? "]) + "Quote: " + "The needs of the many, outweighs the needs of the few"
+    body = generateQuoteQuestion()
     print("Posting tweet...")
     result = api.PostUpdate(body)
 
@@ -72,10 +73,9 @@ def twitter_demo():
 
     # THIS PART DOESNT WORK
     # replying to a tweet:
-    tweet_id = 1229730717636136961
-    # tweet id of the tweet https://twitter.com/jd7h/status/1178660081648492545
-    # body = random.choice(['This was not what I had in mind, ', 'Not exactly bucko,', 'Nice try kiddo, ']) + random.choice(["you are welcome to try again.", "don't give up! It's not that hard.", "just think over it for a minute.", "don’t overthink it just try again."])
-    body = "Its from Star Wars, second episode I believe"
+    tweet_id = 1229754707775827968
+    body = makeReplies(tweet_id)
+    # Add backend answer checker here
     print("Posting reply...")
     result = api.PostUpdate(body, in_reply_to_status_id=tweet_id)
 
@@ -90,5 +90,47 @@ def twitter_demo():
     #   print("Adding ", friend_id)
     #   result = api.CreateListsMember(list_id=mylist.id,user_id=friend_id)
 
+
+
+
+def getReplies(tweet_id):
+    replies = api.GetSearch(raw_query="q=to%3AThatOldMovieGu1&since_id="+str(tweet_id))
+    reply_list = []
+    for t in replies:
+        if t._json["in_reply_to_status_id"] == tweet_id:
+            reply_list.append(t)
+
+    return reply_list
+
+
+def makeReplies(tweet_id, checkAnswer):
+    replies = getReplies(tweet_id)
+    user = ""
+    for t in replies:
+        user = t._json["user"]["screen_name"]
+
+    # If the user answer correctly
+    if checkAnswer == 1:
+        answer = random.choice(
+            ["It was indeed, " + "<movie name>", "Congratulations @" + str(user) + ", you are indeed correct", "BINGO!",
+             "Cheers to @" + str(user) + ", for solving the quote."])
+
+    # If the user answer incorrectly
+    if checkAnswer == 0:
+        answer = random.choice(
+            ['This was not what I had in mind, ', 'Not exactly bucko,', 'Nice try kiddo, ']) + random.choice(["you are welcome to try again.",
+            "don't give up! It's not that hard.", "just think over it for a minute.", "don’t overthink it just try again."])
+
+    # If the user replies a nonexistent movie or replies in gibberish
+    if checkAnswer == 2:
+        answer = "Sorry but I don't know this movie", "Is this an actual movie?", "That doesnt sound familiar", "I think you've made a typo"
+
+    return answer
+
+def generateQuoteQuestion():
+    Quote_of_the_Day = random.choice(["Hey there movie fans, can any of you guys figure out where this quote comes from? ",
+                                      "I have this quote stuck in my head, can someone help me? ", "Can you guess where this quote came from? ",
+                                      "Can someone help me find out where this quote is from? "]) + "Quote: " + "\"What, a swallow carrying a coconut?\" "
+    return Quote_of_the_Day
 
 twitter_demo()
