4d962b9d393679ed262af38df86b3ba32239ddf9
RinaldsKugis <rinalds.kugis@outlook.com>
2020-02-18 15:04:24 +0100

    Able to get replies of a specific tweet. The general timing.

--- frontend.py
+++ frontend.py
@@ -1,21 +1,44 @@
 import twitter
 import json
-from TwitterSearch import *
+from datetime import datetime, timedelta
 
+#Global variables
+# Api Keys added
 api = twitter.Api(consumer_key='77fIyHnx653Nnx0W4Iz4XRua9',
                   consumer_secret='rG0FQNYliP2jlab2Nf0VYm83a3iC0UUFmyOetHd8aaD4nSa4aE',
                   access_token_key='1227546694969167873-6Feip4gF0vg0DJxN38yLWCrPIVNYVt',
                   access_token_secret='rOozbozQIqpy5JonuSrlkxq7d4NXSeIjhUCjtotVgZeHJ')
 
-def twitterBot():
+posting_frequency = 900 #makes post every $posting_frequency$ seconds
+last_tweet_time = datetime.now() - timedelta(seconds=posting_frequency+100)
+previous_tweet_list = []
 
-    # Api Keys added
-    # tweeting
-    tweet_id = 1229730717636136961
-    replies = getReplies(1229730717636136961)
-    for t in replies:
-        print (t._json["user"]["screen_name"])
-    print (replies)
+reply_frequency = 60
+last_reply_check_time =datetime.now() - timedelta(seconds=reply_frequency+100)
+
+def twitterBot():
+    global last_tweet_time,last_reply_check_time, posting_frequency, reply_frequency, previous_tweet_list
+
+    #Checks if it is time to tweet a new tweet
+    if (datetime.now()-last_tweet_time)>timedelta(seconds=posting_frequency):
+        if len(previous_tweet_list)!=0:
+            #Here goes through the previous tweets list and gives answers to all outstanding quotes
+            print ("give answers to previous tweets")
+
+        #Here calls funtion to get new quote
+        print ("Post a new quote")
+
+        last_tweet_time = datetime.now()
+
+    #Check for replies
+    if (datetime.now() - last_reply_check_time) > timedelta(seconds=reply_frequency):
+        for t in previous_tweet_list:
+            #here checks for replies and answers if necessary
+            print ("check for replies")
+
+        last_reply_check_time = datetime.now()
+
+
 
 def getReplies(tweet_id):
     replies = api.GetSearch(raw_query="q=to%3AThatOldMovieGu1&since_id="+str(tweet_id))
